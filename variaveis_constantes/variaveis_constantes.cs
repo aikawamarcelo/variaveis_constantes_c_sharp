b02726240ebd47e694bc399456344afa256cd070
= <=>
2022-07-09 21:52:27 -0300

    Adicionando arquivos do projeto ao commit

---
+++ variaveis_constantes/variaveis_constantes.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace variaveis_constantes
+{
+    class Program
+    {
+        public const int SAIDA_PROGRAMA = 1;
+        static void Main(String[] args)
+        {
+            while (true) { 
+            Console.WriteLine("Digite " + SAIDA_PROGRAMA  + " para sair do programa ");
+            int valor = int.Parse(Console.ReadLine());
+
+            if(SAIDA_PROGRAMA == valor)
+            {
+                break;
+            }
+          }
+       }
+
+    }
+
+    
+}
